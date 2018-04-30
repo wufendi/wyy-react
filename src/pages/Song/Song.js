9b28dc75f3d5ebbde055fd7af2faadae1db04592
wufendi <13501566426@163.com>
2018-04-30 18:24:38 +0800

    song

--- src/pages/Song/Song.js
+++ src/pages/Song/Song.js
@@ -3,14 +3,14 @@ import React, {Component} from 'react';
 import {musicUrl,songDetail} from 'api/allApisList';
 import './style.scss';
 import defaultImg from './imgs/zly.jpg';
-
+const defaultMp3 = require("file-loader!./mp3/xsdy.mp3");
 export default class Song extends Component {
     constructor(props) {
         super(props);
         this.id = this.props.location.query?this.props.location.query.id : '';
         this.state = {
             backgroundImg: defaultImg,
-            musicUrl: 'http://m10.music.126.net/20180429154449/51d0e1869a06bad918c1842f89312934/ymusic/2ba4/2130/8a71/574a2be2df717b2b1b0a57273fd3f873.mp3',
+            musicUrl: defaultMp3/*'http://m10.music.126.net/20180429154449/51d0e1869a06bad918c1842f89312934/ymusic/2ba4/2130/8a71/574a2be2df717b2b1b0a57273fd3f873.mp3'*/,
         };
         this.getMusicUrl = () => {
             musicUrl(this.id).then(response => {
@@ -63,6 +63,7 @@ export default class Song extends Component {
                         <li>ighghh</li>
                         <li>ighghh</li>
                     </ul>
+                    <audio controls="controls" autoPlay="autoplay" src={this.state.musicUrl}/>
                 </div>
                 {/*{*/}
                     {/*this.id ?  <audio controls="controls" autoPlay="autoplay" src={this.state.musicUrl}/> : '页面不存在✌️'*/}
